597c72bf35e95743c4fee8215ac59a542ddec763
Nicholas Chill <11b3827@gmail.com>
2024-10-18 14:25:21 +0200

    Finished main logic

--- main.c
+++ main.c
@@ -15,28 +15,43 @@
 #include <stdlib.h>
 #include <time.h>
 
+void checkWord(char word[], char userWord[]);
+
 int main() {
-    int tries = 6;
+    int guessNum = 1;
     char words [2] [6] = {"water", "space"}; 
-    char userWord[6] = "";
+    char userWord[6];
     srand(time(0));
     int randomNum = rand() % 2;
 
-    printf("Word is: ");
-    printf("%s\n", words[randomNum]);
+    while (guessNum < 7 && strcmp(userWord, words[randomNum]) != 0) {
+        printf("Guess #%d\n", guessNum);
+        printf(" Enter a word: \n");
+        scanf("%s", userWord);
+        checkWord(words[randomNum], userWord);
+        guessNum++;
+        printf("\n");
+    }
 
-    printf("Enter a word: \n");
-    scanf("%s", userWord);
-    checkWord(words[randomNum], userWord);
+    if (strcmp(userWord, words[randomNum]) == 0) {
+        printf("%s", "You Win!");
+    } else {
+        printf("%s", "You Lose!");
+    }
     return 0;
 }
 
 void checkWord(char word[], char userWord[]) {
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
-            if (word[i] == userWord[j]) {
-                printf("match: ");
-                printf("%c\n", userWord[j]);
+            if (userWord[i] == word[j]) {
+                if (i == j) {
+                    printf("%s", "Correct spot: ");
+                    printf("%c\n", userWord[i]);
+                } else {
+                    printf("%s", "Wrong spot: ");
+                    printf("%c\n", userWord[i]);
+                }
             }
         }
     }
